fdd60a6db790c127bcde86f5e5ddc447b2936972
Kavindu07873 <Dilshankavindu07715@gmail.com>
2022-10-13 10:18:58 +0530

    updated

--- Assinments/JS/SPA/Controllers/Orders.js
+++ Assinments/JS/SPA/Controllers/Orders.js
@@ -5,17 +5,20 @@ function loadCustomersIDOption() {
     for (let cus of Customer) {
         $("#Cusselectid").append(`<option>${cus.id}</option>`);
     }
-
+    loadDataCus();
 }
 
 
 function loadDataCus(){
 
     //feild walata data load kirima
-    $("#Cusselectid").click(function (){
+    $("#Cusselectid>option").click(function (){
+        alert("mkd la");
+
         let Name =    $(this).children(":eq(1)").text();
         //put data o text field
         $("#CusShowName").val(Name);
+        console.log($("#CusShowName").val(Name))
 
     });
 
